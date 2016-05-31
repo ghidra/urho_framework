ff35f4a22b15e278419136dccbb8edc12e925d52
ghidra <ghidra@gmail.com>
2016-05-30 22:58:08 -0400

    ESc to ESCAPE for head

--- src/ApplicationInput.cpp
+++ src/ApplicationInput.cpp
@@ -240,7 +240,7 @@ void ApplicationInput::HandleKeyDown(StringHash eventType, VariantMap& eventData
     int key = eventData[P_KEY].GetInt();
 
     // Close console (if open) or exit when ESC is pressed
-    if (key == KEY_ESC)
+    if (key == KEY_ESCAPE)
     {
         Console* console = GetSubsystem<Console>();
         if (console->IsVisible())
